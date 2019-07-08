a417a0d02973b16a8674570a0b4499d9176cc58a
Jack Zhang <jack.zhang@pepperstone.com>
2019-07-08 16:32:12 +1000

    setup nuxt

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+  extends: [
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: [
+    'prettier',
+    '@typescript-eslint'
+  ],
+  // add your custom rules here
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    'vue/html-self-closing': ['error', {
+      'html': {
+        'void': 'always',
+        'normal': 'never',
+        'component': 'always'
+      },
+      'svg': 'always',
+      'math': 'always'
+    }]
+  }
+}
